08271af4de2c5cb60ab827984aa13c9ddce4ad1c
Dustin Doiron <dustin@dustindoiron.com>
2013-08-29 15:56:30 -0700

    Add back in /exit and /alive API calls

--- api.go
+++ api.go
@@ -3,31 +3,46 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+	"fmt"
 )
 
 /**
  * Starts the API endpoint webservice
  */
 func StartHttpApi() error {
-	log.Printf("Publishing service available on port %s.", HARE_API_PORT_PUBLISHING)
-	log.Print("Statistics service available at /stats.")
+	log.Printf("API service available on port %s.", HARE_API_PORT_PUBLISHING)
+
 	http.HandleFunc("/", apiRequestPublish)
 	http.HandleFunc("/stats", apiRequestStats)
+	http.HandleFunc("/alive", apiRequestAlive)
+	http.HandleFunc("/exit", apiRequestExit)
+
 	http.HandleFunc("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 	})
+
 	err := http.ListenAndServe(":"+HARE_API_PORT_PUBLISHING, nil)
 	if err != nil {
 		return err
 	}
-	log.Print("shouldnt be here")
+
 	return nil
 }
 
 /**
- * Prepares and dispatches a response via ResponseWriter for an exit request
+ * Exits hare upon receiving a /exit API call
  */
 func apiRequestExit(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Exit command received by %s.", r.RemoteAddr)
-	//  os.Exit(2)
+	os.Exit(2)
+}
+
+/**
+ * Responds with a success health-check body, 200 HTTP status code upon receiving an /alive API call
+ */
+func apiRequestAlive(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Alive")
+	w.WriteHeader(http.StatusOK)
+	return
 }
